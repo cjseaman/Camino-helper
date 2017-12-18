5d9c5f216d2322457fd2790564683220fe0d9f1b
Collin Seaman <collin2009@gmail.com>
2017-12-17 22:02:15 -0800

    Added documentation for all functions

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
+//Collin Seaman
+//
+//Camino Word Counter
+//
+//Counts words inside comment box, displays word count as a number updated every .5 sec, and shows average word count of all other posts.
+
 var iframeBox;
 var iframeDocBox;
 var iframeDocText;
@@ -9,10 +15,15 @@ var postText;
 var counterNode;
 var otherText = [];
 
+//main function runs every 3 seconds until findDoc() is initialized (comment page is loaded)
 
 setTimeout(function() {
+
+	//sets averageWordCount() to the number of total words in all other comments divided by the number of comments
 	
 	var averageWordCount = countOtherWords();
+
+	//Once findDoc() is initialized, function runs every 1 seconds, searching for the comment box. Once comment box is found, clears findDoc() interval and runs findText()
 
 	var findDoc = window.setInterval(function(){
 		if((iframeBox = document.querySelector('[title="Rich Text Area. Press ALT+F8 for help"]')) != null) {
@@ -20,6 +31,8 @@ setTimeout(function() {
 			iframeDocBox = doc.getElementById('tinymce');
 			clearInterval(findDoc);
 			
+			//Gets html from iframe comment box as a string and passes it to countWords()
+
 			var findText = window.setInterval(function() {
 				iframeDocText = getAllWords(iframeDocBox);
 				wordCount = countWords(iframeDocText);
@@ -37,6 +50,8 @@ function getAllWords(container) {
     return allInternal;
 }
 
+//counts words given a string s of characters
+
 function countWords(s){
 	if(s.match(/[a-z]/i)) {
 		s = s.replace(/(^\s*)|(\s*$)/gi,"");
@@ -46,6 +61,8 @@ function countWords(s){
 		return s.split(' ').length;
 	} else return 0;
 }
+
+//Finds all posts on page and returns the average word count of those posts
 
 function countOtherWords() {
 	var count = 0;
